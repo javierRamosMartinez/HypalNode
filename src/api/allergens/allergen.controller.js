86b4d86bcf2a46af38b80e76abe4d01936b57a27
TinaJS <cristinalambardeta@gmail.com>
2024-05-20 12:06:19 +0200

    addManyAllergens

--- src/api/allergens/allergen.controller.js
+++ src/api/allergens/allergen.controller.js
@@ -24,6 +24,53 @@ const addAllergen = async (req, res, next) => {
     });
   }
 };
+const addManyAllergens = async (req, res) => {
+  try {
+    const allergens = req.body;
+
+    // Array para almacenar alérgenos que ya existen
+    const existingAllergens = [];
+
+    // Array para almacenar alérgenos que son nuevos y se pueden insertar
+    const newAllergens = [];
+
+    // Verificar si cada alérgeno ya existe en la base de datos
+    for (const allergen of allergens) {
+      const existingAllergen = await Allergen.findOne({ name: allergen.name });
+
+      // Si el alérgeno ya existe, agregarlo al array de alérgenos existentes
+      if (existingAllergen) {
+        existingAllergens.push(existingAllergen);
+      } else {
+        // Si el alérgeno no existe, agregarlo al array de alérgenos nuevos
+        newAllergens.push(allergen);
+      }
+    }
+
+    // Si hay alérgenos existentes, devolver un error
+    if (existingAllergens.length > 0) {
+      return res.status(400).json({
+        status: 400,
+        message: "Some allergens already exist",
+        existingAllergens: existingAllergens,
+      });
+    }
+
+    // Si no hay alérgenos existentes, insertar los alérgenos nuevos
+    const insertedAllergens = await Allergen.insertMany(newAllergens);
+    res.status(201).json({
+      status: 201,
+      message: "Allergens inserted successfully",
+      data: insertedAllergens,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 500,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
 const getOneAllergen = async (req, res, next) => {
   try {
     const allergen = await Allergen.findById(req.params.id);
@@ -63,6 +110,7 @@ const getAllAllergens = async (req, res, next) => {
 
 module.exports = {
   addAllergen,
+  addManyAllergens,
   getOneAllergen,
   updateAllergen,
   removeAllergen,
